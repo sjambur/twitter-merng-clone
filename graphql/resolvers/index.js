645ebfedaebad97b064644c28d9b55e255529d18
Sachin Jambur <sachin.jambur@neofinancial.com>
2021-06-01 19:44:47 -0600

    initial commit

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,15 @@
+const { modelName } = require("../../models/Post");
+const postsResolvers = require("./posts");
+const usersResolvers = require("./users");
+const commentsResolvers = require("./comments");
+
+module.exports = {
+  Query: {
+    ...postsResolvers.Query,
+  },
+  Mutation: {
+    ...usersResolvers.Mutation,
+    ...postsResolvers.Mutation,
+    ...commentsResolvers.Mutation,
+  },
+};
